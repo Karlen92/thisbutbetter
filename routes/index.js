7c29f28106dcb0635cb381bbe92d32e674929e1b
Karlen92 <mkkarlen92@gmail.com>
2019-04-10 18:01:17 +0400

    Initial Commit

---
+++ routes/index.js
@@ -0,0 +1,40 @@
+var express = require('express');
+const request = require("request");
+const axios = require('axios');
+const cheerio = require('cheerio');
+const fs = require('fs');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  debugger
+  res.sendFile('index.html');
+
+   // https://www.reactgeeks.com/
+});
+
+/* GET home page. */
+router.get('/proxy', async function(req, res, next) {
+        try {
+            const response = await axios.get('https://www.reactgeeks.com/');
+            if (response.status === 200) {
+                const html = response.data;
+                const $ = cheerio.load(html);
+                debugger;
+                // Get text
+                $("body").append("<script src='https://cdnjs.cloudflare.com/ajax/libs/highstock/6.0.3/adapters/standalone-framework.js'></script>");
+                console.log("------- with axios module -------")
+                console.log($.text());
+                // Get HTML
+                //console.log($.html());
+                res.set('Content-Type', 'text/html');
+                res.send($.html());
+            }
+        }catch(err){
+            throw new Error(err);
+        }
+    // https://www.reactgeeks.com/
+});
+
+
+module.exports = router;
