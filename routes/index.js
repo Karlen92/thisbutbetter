d2a4de4870a98f6e5787c48bbb5f947fb0dc2e01
Karlen92 <mkkarlen92@gmail.com>
2019-04-19 18:06:52 +0400

    Bug fixings

--- routes/index.js
+++ routes/index.js
@@ -1,82 +1,42 @@
-var express = require('express');
+const fs = require('fs');
+const express = require('express');
 const request = require("request");
 const axios = require('axios');
 const cheerio = require('cheerio');
 var proxy = require('express-http-proxy');
-const fs = require('fs');
-var router = express.Router();
+const router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  debugger
+
   res.sendFile('index.html');
 
    // https://www.reactgeeks.com/
 });
 
+const database = {
+    'geeks':"https://www.reactgeeks.com/",
+    'hnoon':"https://hackernoon.com/",
+
+}
+
 /* GET home page. */
-router.get('/proxy', async function(req, res, next) {
+router.get('/proxy/*/end', async function(req, res, next) {
         try {
-            const response = await axios.get('https://www.reactgeeks.com/');
+            const response = await axios.get(database[req.params[0].replace("/",'')]);
             if (response.status === 200) {
                 const html = response.data;
                 const $ = cheerio.load(html);
+                //Try using react
+                const jsContent=fs.readFileSync("../ui-core/dom/index.js", "utf8");
+                const cssContent=fs.readFileSync("../ui-core/styles/index.css", "utf8");
                 // Get text
-                /////////////////////////////////// STYLES  //////////////////////////////////////////////
-                $("body").append("<style> .thisButBetterOutline {\n" +
-                    "    outline: 2px dashed #658bff !important\n" +
-                    "}</style>");
-                ////////////////////////////////////    JS    /////////////////////////////////////////////
-                $("body").append("<script type=\"text/javascript\" >var ThisButBetter = {};\n" +
-                    "\n" +
-                    "ThisButBetter.getElementPath = function(node){\n" +
-                    "    const parts = [];\n" +
-                    "    while (node.parentElement) {\n" +
-                    "        var str = node.tagName;\n" +
-                    "        if (node.id) {\n" +
-                    "            str += `#${node.id}`;\n" +
-                    "            parts.unshift(str);\n" +
-                    "            break;\n" +
-                    "        }\n" +
-                    "        var siblingsArr = Array.prototype.slice.call(node.parentElement.childNodes);\n" +
-                    "        var ind = siblingsArr.filter(n=> n.attributes != null).indexOf(node);\n" +
-                    "        parts.unshift(str + `:nth-child(${ind + 1})`);\n" +
-                    "        node = node.parentElement;\n" +
-                    "    }\n" +
-                    "    return parts.join(' > ');\n" +
-                    "};\n" +
-                    "\n" +
-                    "ThisButBetter.hoverHandlerFactory = function(params){\n" +
-                    "\n" +
-                    "    return function(el){\n" +
-                    "        const current = $('.' + params.outline).removeClass(params.outline);\n" +
-                    "        $(el.target).addClass(params.outline);\n" +
-                    "    };\n" +
-                    "};\n" +
-                    "\n" +
-                    "ThisButBetter.clickHandlerFactory = function(params){\n" +
-                    "    return function(ev){\n" +
-                    "        var x = ev.pageX - $(ev.target).offset().left;\n" +
-                    "        var y = ev.pageY - $(ev.target).offset().top;\n" +
-                    "        var circleRadius = 30;\n" +
-                    "        console.log(x);\n" +
-                    "        console.log(y);\n" +
-                    "        event.stopPropagation();\n" +
-                    "        // var pos = $(ThisButBetter.getElementPath(ev.target)).position();\n" +
-                    "        var pos = {\n" +
-                    "            top: $(ev.target).offset().top + y - circleRadius/2,\n" +
-                    "            left:$(ev.target).offset().left + x- circleRadius/2\n" +
-                    "        };\n" +
-                    "\n" +
-                    "        $('body').append(\"<div style='z-index:9999;border-radius:15px;width:\" + circleRadius + \"px;height:\" + circleRadius + \"px;background-color:#53a267;position: absolute;top: \"+ pos.top + \"px;left: \" + pos.left +\"px'>\");\n" +
-                    "    };\n" +
-                    "};\n" +
-                    "\n" +
-                    "$(\"body *\").hover(ThisButBetter.hoverHandlerFactory({outline: 'thisButBetterOutline'}));\n" +
-                    "\n" +
-                    "$(\"body *\").click(ThisButBetter.clickHandlerFactory());\n</script>");
-                // Get HTML
-                //console.log($.html());
+                /************************ Injections  ************************/
+                $("body")
+                    .append("<style>" + cssContent + "</style>")
+                    .append("<script type=\"text/javascript\" > " + jsContent + "</script>");
+
+                /********************** Injections end  **********************/
                 res.set('Content-Type', 'text/html');
                 res.send($.html());
             }
@@ -86,7 +46,14 @@ router.get('/proxy', async function(req, res, next) {
     // https://www.reactgeeks.com/
 });
 
-router.get('/*',  proxy('https://www.reactgeeks.com'));
+router.get('/proxy/*/end/*',  proxy('https://www.reactgeeks.com/', {
+    proxyReqPathResolver: function (req) {
+        var parts = req.url.split('/end');
+        var queryString = parts[1];
+        var updatedPath = parts[0].replace(/test/, 'tent');
+        return parts[1];
+    }
+}));
 
 router.post('/*',  proxy('https://www.reactgeeks.com'));
 
--- ui-core/dom/index.js
+++ ui-core/dom/index.js
@@ -1,5 +1,12 @@
 var ThisButBetter = {};
 
+
+ThisButBetter.sendMessageToParent = function(data){
+    var myEvent = new CustomEvent('this_but_better_iframe_event', { detail: { data } });
+    window.parent.dispatchEvent(myEvent);
+    //iframeEl.contentWindow.postMessage('', '*');
+};
+
 ThisButBetter.getElementPath = function(node){
     const parts = [];
     while (node.parentElement) {
@@ -10,9 +17,10 @@ ThisButBetter.getElementPath = function(node){
             break;
         }
         var siblingsArr = Array.prototype.slice.call(node.parentElement.childNodes);
-        var ind = siblingsArr.filter(n=> n.attributes != null).indexOf(node);
+        var ind = siblingsArr.filter(n => n.attributes != null).indexOf(node);
         parts.unshift(str + `:nth-child(${ind + 1})`);
         node = node.parentElement;
+
     }
     return parts.join(' > ');
 };
@@ -32,16 +40,21 @@ ThisButBetter.clickHandlerFactory = function(params){
         var circleRadius = 30;
         console.log(x);
         console.log(y);
+        ThisButBetter.sendMessageToParent({x, y});
         event.stopPropagation();
         // var pos = $(ThisButBetter.getElementPath(ev.target)).position();
         var pos = {
             top: $(ev.target).offset().top + y - circleRadius/2,
-            left:$(ev.target).offset().left + x- circleRadius/2
+            left:$(ev.target).offset().left + x - circleRadius/2
         };
 
         $('body').append("<div style='z-index:9999;border-radius:15px;width:" + circleRadius + "px;height:" + circleRadius + "px;background-color:#53a267;position: absolute;top: "+ pos.top + "px;left: " + pos.left +"px'>");
     };
 };
+
+window.addEventListener('this_but_better_parent_event', function(e) {
+    alert(e.detail.data);
+}, false);
 
 $("body *").hover(ThisButBetter.hoverHandlerFactory({outline: 'thisButBetterOutline'}));
 
